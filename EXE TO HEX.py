48a049d5227020dece0206b9344f1bb81f72a801
Moésio Fiùza <93868671+MoesioFiuza@users.noreply.github.com>
2024-06-29 19:50:03 -0300

    Add files via upload

---
+++ EXE TO HEX.py
@@ -0,0 +1,29 @@
+def ler_exe_como_binario(caminho_arquivo):
+    try:
+        with open(caminho_arquivo, 'rb') as arquivo:
+            dados_binarios = arquivo.read()
+            return dados_binarios
+    except FileNotFoundError:
+        print(f"Arquivo não encontrado: {caminho_arquivo}")
+        return None
+    except IOError:
+        print(f"Erro ao ler o arquivo: {caminho_arquivo}")
+        return None
+
+def escrever_hexadecimal_em_arquivo(dados_binarios, caminho_saida):
+    representacao_hex = dados_binarios.hex()
+    with open(caminho_saida, 'w') as arquivo_saida:
+        for i in range(0, len(representacao_hex), 32):
+            linha = representacao_hex[i:i+32] + '\n'
+            arquivo_saida.write(linha)
+
+def main():
+    caminho_arquivo = r'C:\Program Files (x86)\Rockstar Games\Social Club\SocialClubHelper.exe'
+    caminho_saida = r'C:\Users\maest\OneDrive\Área de Trabalho\KEK\social_hex.txt'
+    dados_binarios = ler_exe_como_binario(caminho_arquivo)
+    if dados_binarios:
+        escrever_hexadecimal_em_arquivo(dados_binarios, caminho_saida)
+        print(f"Código hexadecimal salvo em: {caminho_saida}")
+
+if __name__ == "__main__":
+    main()
